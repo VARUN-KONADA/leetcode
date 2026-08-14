67bcae8eff4b22ece546d1a34ccb86ec37f6cabc
Varun <konadavarun2008@gmail.com>
2026-08-14 13:40:31 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10 MB (69.47%)

--- 0066-plus-one/solution.c
+++ 0066-plus-one/solution.c
@@ -5,19 +5,21 @@ int* plusOne(int* digits, int digitsSize, int* returnSize)
 {
     int* op = malloc(digitsSize*sizeof(int));
     int update = digitsSize;
-    for (int i = 0; i < digitsSize; i++)
+    for (int i = 0; i < digitsSize; i++)    //coping digits
     {
         op[i] = digits[i];
     }
+    
     do
     {
         update--;
         if(update + 1 != digitsSize && op[update + 1] == 10)
+            //  if 9+1 done in before step, makes it 0
         {
             op[update+1] = 0;
-
         }
-        if(update == -1)
+
+        if(update == -1)    //if array exceeds length
         {
             op = realloc(op, (digitsSize + 1) * sizeof(int));
             memmove(&op[1], &op[0], (digitsSize) * sizeof(int));
